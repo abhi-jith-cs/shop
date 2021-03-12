03116ddb067ed5cf31811958ee3d07dfa1a3c01a
abhijith <abhijithcs10@gmail.com>
2021-03-12 16:28:02 +0530

    added homepage layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
+import "./App.css";
+import Homepage from "./homepage.component";
+import "./homepage.styles.scss";
 
 function App() {
   return (
     <div>
-      <h1>Hello world</h1>
-      
+      <Homepage />
     </div>
   );
 }
