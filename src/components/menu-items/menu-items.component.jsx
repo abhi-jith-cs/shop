aef3192330d80bacd0b7f31d217bce90e03368cb
abhijith <abhijithcs10@gmail.com>
2021-03-15 13:23:14 +0530

    added bg image and added components

---
+++ src/components/menu-items/menu-items.component.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./menu-items.styles.scss";
+
+const MenuItems = ({ title, imageUrl, size }) => (
+  <div
+    style={{
+      backgroundImage: `url(${imageUrl})`,
+    }}
+    className={`${size} menu-items`}
+  >
+    <div className="content">
+      <h1 className="title">{title}</h1>
+      <span className="subtitle">Shop Now</span>
+    </div>
+  </div>
+);
+export default MenuItems;
